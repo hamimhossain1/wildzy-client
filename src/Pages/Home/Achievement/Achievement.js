547da849fe0f24f48d6299275cc22c8b41dbc228
HamimHossain <hamim.mgp@gmail.com>
2022-11-11 00:00:38 +0600

    responsive almost done

--- src/Pages/Home/Achievement/Achievement.js
+++ src/Pages/Home/Achievement/Achievement.js
@@ -3,19 +3,19 @@ import funFactsImg from '../../../Assets/funfactsImg/achivement-img.jpg'
 
 const Achievement = () => {
     return (
-        <div className="hero mb-24" style={{ backgroundImage: `url(${funFactsImg})` }}>
+        <div className="hero mb-24 " style={{ backgroundImage: `url(${funFactsImg})` }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="py-12">
-                    <h1 className="mb-5 text-5xl font-bold">Funfacts You should Know</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="mb-5 text-3xl md:text-3xl lg:text-5xl font-bold">Funfacts You should Know</h1>
+                    <p className="mb-5 w-10/12 md:w-9/12 lg:w-10/12 mx-auto">Wildzy has an incredible achievement  as photography service and got some amazing awards for the eye catching photograph. Wildzy service is very  professional, but knows how to help you relax in front of the camera and keep things relaxed. Wildzy is always quick to reply and we got all of our engagement photos back earlier than expected.”</p>
 
-                    <div className='flex justify-between w-8/12 mx-auto mt-12'>
-                        <div>
+                    <div className='md:flex justify-between w-8/12 mx-auto mt-12'>
+                        <div className='mb-8'>
                             <h1 className='text-4xl'><span className='text-yellow-400 font-bold'>77</span> +</h1>
                             <p>Years of Experience</p>
                         </div>
-                        <div>
+                        <div className='mb-8'>
                             <h1 className='text-4xl'><span className='text-yellow-400 font-bold'>358</span> +</h1>
                             <p>Territory Explore</p>
                         </div>
